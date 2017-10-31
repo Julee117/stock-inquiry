95324a4b56b0bd715ddef6e88e53994f541955b1
Julee117 <juliannne@gmail.com>
2017-10-31 16:30:45 -0400

    create menu

--- lib/stock_inquiry/cli.rb
+++ lib/stock_inquiry/cli.rb
@@ -22,4 +22,48 @@ class StockInquiry::CLI
       obtain_ticker
     end
   end
+
+  def menu
+    input = ""
+    while input != "exit"
+      puts ""
+      puts "#{ticker}"
+      puts ""
+      puts "Enter CURRENT to see current price"
+      puts "Enter OPEN to see open price"
+      puts "Enter PREVIOUS to see previous close price"
+      puts "Enter RANGE to see 52 week price range from low to high"
+      puts "Enter CHART to see historical price chart"
+      puts "Enter ARTICLES to see related news articles"
+      puts "Enter DESCRIPTION to read about the company"
+      puts "Enter MORE if you would like to do further research"
+      puts "Enter NEW to research another stock/security"
+      puts "Enter EXIT to exit program"
+
+      input = gets.downcase.strip
+
+      case input
+      when "current"
+        list_current_price
+      when "open"
+        list_open_price
+      when "previous"
+        list_previous_price
+      when "range"
+        list_range
+      when "chart"
+        show_chart
+      when "articles"
+        list_articles
+      when "description"
+        show_description
+      when "more"
+        more
+      when "new"
+        restart
+      when "exit"
+        exit_program
+      end 
+    end
+  end
 end
