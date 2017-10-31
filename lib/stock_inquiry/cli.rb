c4f66f2f621d20acd4fbc99e40a9729792505a00
Julee117 <juliannne@gmail.com>
2017-10-31 16:22:04 -0400

    create method to validate ticker

--- lib/stock_inquiry/cli.rb
+++ lib/stock_inquiry/cli.rb
@@ -1,6 +1,25 @@
 class StockInquiry::CLI
+  attr_reader :ticker, :ticker_list
+
+  def initialize
+    @ticker_list = File.readlines("./lib/stock_inquiry/stock_ticker_092017.txt").map { |ticker| ticker.chomp }.sort
+  end
 
   def start
-    
+    obtain_ticker
+    valid_ticker?
+  end
+
+  def obtain_ticker
+    puts "Please enter stock/security ticker symbol"
+    @ticker = gets.upcase.strip
+  end
+
+  def valid_ticker?
+    match_ticker = ticker_list.select { |stock| stock == "#{ticker}" }
+    if match_ticker == []
+      puts "This is not a valid ticker"
+      obtain_ticker
+    end
   end
 end
